805f33fa11828b8760980c7f3ce52081444a1f57
pabloascencao <pablo.ascencao@globant.com>
2018-06-07 22:59:44 -0300

    trying to deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,18 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.DOMAIN,
-    databaseURL: process.env.URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.BUCKET,
-    messagingSenderId: process.env.SENDER_ID
+    // apiKey: process.env.API_KEY,
+    // authDomain: process.env.DOMAIN,
+    // databaseURL: process.env.URL,
+    // projectId: process.env.PROJECT_ID,
+    // storageBucket: process.env.BUCKET,
+    // messagingSenderId: process.env.SENDER_ID
+
+    apiKey: 'AIzaSyBGjqtpvYwQesyUNXiZ4nktNprvnKYMWEg',
+    authDomain: 'todohogar-10387.firebaseapp.com',
+    databaseURL: 'https://todohogar-10387.firebaseio.com',
+    projectId: 'todohogar-10387',
+    storageBucket: 'todohogar-10387.appspot.com',
+    messagingSenderId: '687651680270'
   }
 };
