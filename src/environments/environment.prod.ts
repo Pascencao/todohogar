013fba253d3a862e30c2c36a1de3be0e32f8b86b
pabloascencao <pablo.ascencao@globant.com>
2018-06-07 22:44:24 -0300

    trying to deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.DOMAIN,
+    databaseURL: process.env.URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.BUCKET,
+    messagingSenderId: process.env.SENDER_ID
+  }
 };
