cb9a9fc75dddfdbae9d31308fb47d251f3d0f36a
pabloascencao <pablo.ascencao@globant.com>
2018-06-18 23:05:12 -0300

    bug fixing

--- src/app/product-config-item/product-config-item.component.ts
+++ src/app/product-config-item/product-config-item.component.ts
@@ -14,6 +14,7 @@ export class ProductConfigItemComponent implements OnInit {
     title: string,
     description: string,
     shortDescription: string,
+    discount: string,
     price: string,
     image: string,
     labels: [string],
